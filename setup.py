7eb5e3008e44f525925946583d225f4ceb197717
Konstantin Nazarov <mail@racktear.com>
2018-11-30 19:02:14 +0300

    Ship to PyPi

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='tarantoolup',
+    packages=[''],
+    version='0.1.0',
+    description='Tarantool instance manager',
+    author='Konstantin Nazarov',
+    author_email='mail@knazarov.com',
+    url='https://github.com/tarantool/tarantoolup',
+    keywords=['tarantool'],
+    classifiers=[],
+    scripts=['tarantoolup']
+)
